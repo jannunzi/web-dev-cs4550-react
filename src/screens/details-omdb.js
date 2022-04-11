b47d99047f7fe46358faa88bcfac35decc491193
JoseAnnunziato <jose.annunziato@sonos.com>
2022-04-11 17:18:16 -0400

    omdb details

--- src/screens/details-omdb.js
+++ src/screens/details-omdb.js
@@ -5,15 +5,32 @@ import axios from "axios";
 
 const DetailsOmdb = () => {
   const [movieDetails, setMovieDetails] = useState({})
+  const [ourMovieDetails, setOurMovieDetails] = useState({})
   const OMDB_URL = 'https://www.omdbapi.com/?apikey=852159f0&i'
   const {imdbID} = useParams()
-  const fetchMovieByImdbID = async () => {
+  const fetchMovieByImdbIDFromOmdb = async () => {
     const response = await axios(`${OMDB_URL}=${imdbID}`)
     setMovieDetails(response.data)
   }
+  const fetchMovieByImdbIDFromOurApi = async () => {
+    const response = await axios(`http://localhost:4000/api/movies/${imdbID}`)
+    setOurMovieDetails(response.data)
+  }
   useEffect(() => {
-    fetchMovieByImdbID()
+    fetchMovieByImdbIDFromOmdb()
+    fetchMovieByImdbIDFromOurApi()
   }, [])
+
+  const handleLikes = () => {
+    console.log(movieDetails)
+    const movie = {
+      title: movieDetails.Title,
+      poster: movieDetails.Poster,
+      imdbID: movieDetails.imdbID
+    }
+    axios.post("http://localhost:4000/api/likes", movie)
+  }
+
   return (
     <div>
       <h1>{movieDetails.Title}</h1>
@@ -28,7 +45,9 @@ const DetailsOmdb = () => {
       {/*{movieDetails.Actors.split(',')*/}
       {/*  .map(actor => <li>{actor}</li>)}*/}
       {/*</ul>*/}
-      <button>Like</button>
+      <button onClick={handleLikes}>
+        Like ({ourMovieDetails.likes})
+      </button>
       <button>Dislike</button>
       <h2>Leave a comment</h2>
       <textarea></textarea>
